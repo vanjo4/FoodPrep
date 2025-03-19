5cd2462c620ba93ed66c7cc1048e385107478b62
vanjo4 <vanshvjoshi@gmail.com>
2025-03-19 21:57:00 +0530

    added routes to webpages and updated

--- adminFrontEnd/src/App.jsx
+++ adminFrontEnd/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import {Routes,Route} from 'react-router-dom'
+import Add from './screens/Add/Add'
+import Orders from './screens/Orders/Orders'
+import List from './screens/List/List'
 
 const App = () => {
   return (
@@ -9,7 +13,12 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
-
+        <Routes>
+          <Route path ="/" element={<Add/>}></Route>
+          <Route path ="/add" element={<Add/>}></Route>
+          <Route path='/list' element={<List/>}></Route>
+          <Route path='/orders' element={<Orders/>}></Route>
+        </Routes>
       </div>
     </div>
   )
--- adminFrontEnd/src/screens/Orders/Orders.jsx
+++ adminFrontEnd/src/screens/Orders/Orders.jsx
@@ -4,7 +4,7 @@ import './Orders.css'
 const Orders = () => {
   return (
     <div>
-      
+      Orders
     </div>
   )
 }
