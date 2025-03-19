0950014e9098e38dfe005bb4329450b4990164dd
vanjo4 <vanshvjoshi@gmail.com>
2025-03-19 20:43:01 +0530

    Navbar Component updated

--- adminFrontEnd/src/App.jsx
+++ adminFrontEnd/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+
+      </div>
+    </div>
   )
 }
 
