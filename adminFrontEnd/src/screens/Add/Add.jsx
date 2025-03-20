fa3da37d9198b005bd1b82608d5a5d4548f42118
vanjo4 <vanshvjoshi@gmail.com>
2025-03-20 20:58:39 +0530

    updated App.css

--- adminFrontEnd/src/screens/Add/Add.jsx
+++ adminFrontEnd/src/screens/Add/Add.jsx
@@ -6,24 +6,24 @@ const Add = () => {
   return (
     <div className='screen'>
       <div className="container">
-        <form >
-          <div className="add-img-upload">
+        <form className='flex-col'>
+          <div className="add-img-upload flex-col">
             <p>Upload Image</p>
             <label htmlFor="image">
               <img src={assets.upload_area} alt="" />
             </label>
             <input type="file" id='image' hidden required />
           </div>
-          <div className="add-product-name">
+          <div className="add-product-name flex-col">
             <p>Name</p>
             <input type="text" placeholder='Type here' />
           </div>
-          <div className="add-product-desc">
-            <p>Name</p>
+          <div className="add-product-desc flex col">
+            <p>Description</p>
             <textarea rows='6' placeholder='Write content here'></textarea>
           </div>
           <div className="add-category-price">
-            <div className="add-category">
+            <div className="add-category flex-col">
               <p>Category</p>
               <select  >
                 <option value="Salad">Salad</option>
@@ -36,7 +36,7 @@ const Add = () => {
                 <option value="Noodles">Noodles</option>
               </select>
             </div>
-            <div className="add-price">
+            <div className="add-price flex-col">
               <p>Price</p>
               <input type="Number" placeholder='₹150'/>
             </div>
